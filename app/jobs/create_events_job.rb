68bc85834c30df9c29766f5329792d86a511dc92
David Sherline <david@hypsol.com>
2017-02-06 10:29:21 -0600

    Refactor process method

--- app/jobs/create_events_job.rb
+++ app/jobs/create_events_job.rb
@@ -2,12 +2,16 @@ class CreateEventsJob < ApplicationJob
   queue_as :default
 
   def perform(date)
-    return false if date_invalid?(date)
-    WikiScraper::EventsCreator.new(date).create
+    create_event(date)
     true
   end
 
   private
+
+  def create_event(date)
+    return false if date_invalid?(date)
+    WikiScraper::EventsCreator.new(date).create
+  end
 
   def date_invalid?(date)
     Day.exists?(date: date) || date.to_date >= Date.current
--- spec/jobs/create_events_job_spec.rb
+++ spec/jobs/create_events_job_spec.rb
@@ -6,16 +6,26 @@ RSpec.describe CreateEventsJob, type: :job do
       let(:day) { create(:day, :today) }
       let(:date) { day.date.to_s }
 
+      before(:each) do
+        CreateEventsJob.perform_now(date)
+      end
+
       it 'should not run the process' do
-        expect(CreateEventsJob.perform_now(date)).to be(false)
+        expect_any_instance_of(WikiScraper::EventsCreator)
+          .to_not receive(:create)
       end
     end
 
     context 'with a date in the future' do
       let(:date) { Date.current + 7.days }
 
+      before(:each) do
+        CreateEventsJob.perform_now(date)
+      end
+
       it 'should not run the process' do
-        expect(CreateEventsJob.perform_now(date)).to be(false)
+        expect_any_instance_of(WikiScraper::EventsCreator)
+          .to_not receive(:create)
       end
     end
 

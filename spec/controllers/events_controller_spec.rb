17564d3d4dc4705f8617c5115e218987e07b458b
David Sherline <david@hypsol.com>
2017-02-06 10:37:50 -0600

    Change search date for test

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
   describe 'GET #index' do
-    let(:date) { '2017-1-1' }
+    let(:date) { '2017-1-15' }
 
     before(:each) do
       stub_request(:any, /en.wikipedia.org/)

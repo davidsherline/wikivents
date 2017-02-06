fa6095ae916f479af60d86d355a28a84915a6e94
David Sherline <david@hypsol.com>
2017-02-06 10:29:52 -0600

    Stub external requests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
+  describe 'GET #index' do
+    let(:date) { '2017-1-1' }
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
+    before(:each) do
+      stub_request(:any, /en.wikipedia.org/)
+        .to_return(body: File.new('spec/fixtures/files/summary.html'),
+                   status: 200)
+
+      get :index, params: { date: date }
+    end
+
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
   end
-
 end
